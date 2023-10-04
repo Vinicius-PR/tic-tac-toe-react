303471901a7276a40f1cafd4ae129ec25a96d5df
Vinicius-PR <vinicius_auto@outlook.com>
2023-10-04 09:32:37 -0300

    first commit

---
+++ src/styles/theme/default.ts
@@ -0,0 +1,12 @@
+const defaultTheme = {
+  lightBlue: '#31C3BD',
+  lightBlueHover: '#65E9E4',
+  lightYellow: '#F2B137',
+  lightYellowHover: '#FFC860',
+  darkNavy: '#1A2A33',
+  semiDarkNavy: '#1F3641',
+  silver: '#A8BFC9',
+  silverHover: '#DBE8ED'
+}
+
+export default defaultTheme;
